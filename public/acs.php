50b7a363634f8f8eda823cd2017406d1ad89b378
Andrew Barron <hello@awmb.uk>
2021-12-10 12:25:24 +0000

    Fix issue where you may not be redirected to admin page after successful login

--- public/acs.php
+++ public/acs.php
@@ -11,34 +11,37 @@ require_once(dirname(__DIR__).'/settings.php');
 $auth = new \OneLogin\Saml2\Auth($wlabarron_saml_settings);
 
 if (isset($_SESSION) && isset($_SESSION['AuthNRequestID'])) {
-        $requestID = $_SESSION['AuthNRequestID'];
-    } else {
-        $requestID = null;
+    $requestID = $_SESSION['AuthNRequestID'];
+} else {
+    $requestID = null;
+}
+
+$auth->processResponse($requestID);
+
+$errors = $auth->getErrors();
+
+if (!empty($errors)) {
+    echo '<p>',implode(', ', $errors),'</p>';
+    if ($auth->getSettings()->isDebugActive()) {
+        echo '<p>'.$auth->getLastErrorReason().'</p>';
     }
+}
 
-    $auth->processResponse($requestID);
+if (!$auth->isAuthenticated()) {
+    echo "<p>Not authenticated</p>";
+    exit();
+}
 
-    $errors = $auth->getErrors();
+$_SESSION['samlUserdata'] = $auth->getAttributes();
+$_SESSION['samlNameId'] = $auth->getNameId();
+$_SESSION['samlNameIdFormat'] = $auth->getNameIdFormat();
+$_SESSION['samlNameIdNameQualifier'] = $auth->getNameIdNameQualifier();
+$_SESSION['samlNameIdSPNameQualifier'] = $auth->getNameIdSPNameQualifier();
+$_SESSION['samlSessionIndex'] = $auth->getSessionIndex();
+unset($_SESSION['AuthNRequestID']);
 
-    if (!empty($errors)) {
-        echo '<p>',implode(', ', $errors),'</p>';
-        if ($auth->getSettings()->isDebugActive()) {
-            echo '<p>'.$auth->getLastErrorReason().'</p>';
-        }
-    }
-
-    if (!$auth->isAuthenticated()) {
-        echo "<p>Not authenticated</p>";
-        exit();
-    }
-
-    $_SESSION['samlUserdata'] = $auth->getAttributes();
-    $_SESSION['samlNameId'] = $auth->getNameId();
-    $_SESSION['samlNameIdFormat'] = $auth->getNameIdFormat();
-    $_SESSION['samlNameIdNameQualifier'] = $auth->getNameIdNameQualifier();
-    $_SESSION['samlNameIdSPNameQualifier'] = $auth->getNameIdSPNameQualifier();
-    $_SESSION['samlSessionIndex'] = $auth->getSessionIndex();
-    unset($_SESSION['AuthNRequestID']);
-    if (isset($_POST['RelayState']) && \OneLogin\Saml2\Utils::getSelfURL() != $_POST['RelayState']) {
-        $auth->redirectTo($_POST['RelayState']);
-    }
+if (isset($_POST['RelayState']) && \OneLogin\Saml2\Utils::getSelfURL() != $_POST['RelayState']) {
+    $auth->redirectTo($_POST['RelayState']);
+} else {
+    $auth->redirectTo("/admin");
+}
